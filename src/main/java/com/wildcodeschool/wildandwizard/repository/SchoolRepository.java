3a418148eb1ed9039e73ea3bde8b48f4951d38ff
lePitch <pierre.henri.jautard@gmail.com>
2020-06-14 12:35:51 +0200

    school repository extends jpa repo

--- src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/SchoolRepository.java
@@ -1,6 +1,11 @@
 package com.wildcodeschool.wildandwizard.repository;
 
-// TODO : update this interface
+import com.wildcodeschool.wildandwizard.entity.School;
 
-public interface SchoolRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// TODO : update this interface
+@Repository
+public interface SchoolRepository extends JpaRepository<School, Long>{
 }
